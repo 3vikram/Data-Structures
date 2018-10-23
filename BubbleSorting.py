7389bcede2f8dd8741c1d2e1f6cf44d9b26426ae
3vikram <viku.vikram05@gmail.com>
2018-10-24 02:55:35 +0530

    Update BubbleSorting.py

--- BubbleSorting.py
+++ BubbleSorting.py
@@ -1,10 +1,14 @@
 def Bubble_Sort(lst):
     for sorting in range(0, len(lst)-1):
+        flag = False
         for element in range(1, len(lst)):
             if lst[element-1] > lst[element]:
                 swampElement = lst[element-1]
                 lst[element-1] = lst[element]
                 lst[element] = swampElement
+                flag = True
+        if flag == False:
+            break
     return lst
 
 if __name__ == '__main__':
